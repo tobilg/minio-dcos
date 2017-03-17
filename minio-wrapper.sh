9fbbe4325b310672b457085c1b7a746f930a0d57
TobiLG <tobilg@gmail.com>
2017-03-17 13:18:52 +0100

    addded dig loop

--- minio-wrapper.sh
+++ minio-wrapper.sh
@@ -4,33 +4,33 @@
 echo "waiting 5 seconds for dns"
 sleep 5
 
+# print environment
 env
 
-echo "http://master.mesos:8080/v2/apps${MARATHON_APP_ID}"
-
-curl -l -s -H "Content-Type: application/json" http://master.mesos:8080/v2/apps${MARATHON_APP_ID}
-
+# get instance count
 INSTANCES=$(curl -l -s -H "Content-Type: application/json" http://master.mesos:8080/v2/apps${MARATHON_APP_ID} | jq '.app.instances')
 
+# echo instance count
 echo "Instances: $INSTANCES"
 
 # try until DNS is ready
-url="${MARATHON_APP_ID:-.marathon.containerip.dcos.thisdcos.directory}"
+ENDPOINT="${MARATHON_APP_ID:-.marathon.containerip.dcos.thisdcos.directory}"
 
 # wait for all tasks starting
-#for i in {1..20}
-#do#
-#	digs=`dig +short $url`
-#	if [ -z "$digs" ]; then
-#		echo "no DNS record found for $url"
-#	else
-#		# calculate discovery members
-#		members=`echo $digs | sed -e "s/$ip //g" -e 's/ /:5000,/g'`":5000"
+for i in {1..20}
+do
+	digs=`dig +short $url`
+	if [ -z "$digs" ]; then
+		echo "no DNS record found for $url"
+	else
+		# calculate discovery members
+		echo $digs
+		#members=`echo $digs | sed -e "s/$ip //g" -e 's/ /:9000,/g'`":9000"
 #		echo "calculated initial discovery members: $members"
 #		export NEO4J_causalClustering_initialDiscoveryMembers=$members
 #		break
-#	fi
-#   sleep 2
-#done
+	fi
+   	sleep 2
+done
 
 sleep 1000
